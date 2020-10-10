48add3f98f1819948948c94451c767228bd19203
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-11 00:20:10 +0600

    appointment added

---
+++ client/src/components/Appointment/Appointment/Appointment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
+
+const Appointment = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Appointment</h1>
+        </div>
+    );
+};
+
+export default Appointment;
