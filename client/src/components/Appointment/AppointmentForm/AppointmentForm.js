a75e6d78de0fd8c6d3cdd67959032fb2d1c527a3
Md Rana Mahmud <ranamd0@gmail.com>
2020-10-11 07:14:36 +0600

    Insert into database done

--- client/src/components/Appointment/AppointmentForm/AppointmentForm.js
+++ client/src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { useForm } from "react-hook-form";
+
 const customStyles = {
     content: {
         top: '50%',
@@ -12,8 +14,26 @@ const customStyles = {
 };
 Modal.setAppElement('#root')
 
-const AppointmentForm = ({ modalIsOpen, closeModal }) => {
+const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn, date }) => {
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => {
+        data.service = appointmentOn;
+        data.date = date;
+        date.created = new Date();
+        fetch('http://localhost:5000/addAppointment', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(success => {
+                if (success) {
+                    closeModal();
+                    alert('Appointment created successfully')
+                }
+            })
 
+    }
     return (
         <div>
             <Modal
@@ -23,15 +43,50 @@ const AppointmentForm = ({ modalIsOpen, closeModal }) => {
                 contentLabel="Example Modal"
             >
 
-                <h2 >Hello</h2>
-                <button onClick={closeModal}>close</button>
-                <div>I am a modal</div>
-                <form>
-                    <input />
-                    <button>tab navigation</button>
-                    <button>stays</button>
-                    <button>inside</button>
-                    <button>the modal</button>
+                <h2 className="text-center text-brand" >{appointmentOn}</h2>
+                <p className="text-center text-secondary">
+                    <small>ON {date.toDateString()}</small>
+
+                </p>
+                <form className="p-5" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <input type="text" ref={register({ required: true })} name="name" placeholder="Your Name" className="form-control" />
+                        {errors.name && <span className="text-danger">This field is required</span>}
+
+                    </div>
+                    <div className="form-group">
+                        <input type="text" ref={register({ required: true })} name="phone" placeholder="Phone Number" className="form-control" />
+                        {errors.phone && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group">
+                        <input type="text" ref={register({ required: true })} name="email" placeholder="Email" className="form-control" />
+                        {errors.email && <span className="text-danger">This field is required</span>}
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-4">
+
+                            <select className="form-control" name="gender" ref={register({ required: true })} >
+                                <option disabled={true} value="Not set">Select Gender</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Not set">Other</option>
+                            </select>
+                            {errors.gender && <span className="text-danger">This field is required</span>}
+
+                        </div>
+                        <div className="col-4">
+                            <input ref={register({ required: true })} className="form-control" name="age" placeholder="Your Age" type="number" />
+                            {errors.age && <span className="text-danger">This field is required</span>}
+                        </div>
+                        <div className="col-4">
+                            <input ref={register({ required: true })} className="form-control" name="weight" placeholder="Weight" type="number" />
+                            {errors.weight && <span className="text-danger">This field is required</span>}
+                        </div>
+                    </div>
+
+                    <div className="form-group text-right">
+                        <button type="submit" className="btn btn-brand">Send</button>
+                    </div>
                 </form>
             </Modal>
         </div>
